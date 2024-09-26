f4705b7a77c098ab961516d1c0fadede492b62a4
sonich <s.berdnikova@onpeak.ru>
2024-09-26 17:57:50 +0300

    Форма адаптив и валидация

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,9 +8,9 @@
             <input
                 type="text"
                 name="form-name"
-                v-model="inputName"
+                value=""
                 @focusin="labelChange"
-                @focusout="labelChange"
+                @focusout="prepareField"
                 placeholder="Text">
             <span>Имя</span>
           </label>
@@ -18,9 +18,9 @@
             <input
                 type="text"
                 name="form-phone"
-                v-model="inputPhone"
+                value=""
                 @focusin="labelChange"
-                @focusout="labelChange"
+                @focusout="prepareField"
                 data-inputmask="'mask': '+7 (999)-999-99-99'"
                 inputmode="text">
             <span>Телефон</span>
@@ -28,10 +28,10 @@
         </div>
         <div class="form__bottom">
           <div>
-            <input id="agreement" type="checkbox" name="form-agreement">
+            <input id="agreement" type="checkbox" name="form-agreement" value="Y">
             <label for="agreement">Я соглашаюсь</label>
           </div>
-          <button type="button" class="blue-button">Отправить</button>
+          <button type="button" @click="prepareForm" class="blue-button">Отправить</button>
         </div>
       </form>
     </div>
@@ -44,13 +44,6 @@
 
   export default {
     name: "site-form",
-    data() {
-      return {
-        inputName: "",
-        inputPhone: "",
-        inputAgreement: "",
-      }
-    },
     mounted() {
       let phoneElement = document.querySelector('[name="form-phone"]');
       Inputmask().mask(phoneElement);
@@ -58,7 +51,79 @@
     methods: {
       labelChange(event) {
         event.target.parentElement.classList.toggle('active');
+      },
+      toggleClasses(event, addClass = '', removeClass = '') {
+        event.target.classList.add(addClass);
+        event.target.parentElement.classList.add(addClass);
+        event.target.classList.remove(removeClass);
+        event.target.parentElement.classList.remove(removeClass);
+      },
+      addErrorElement(event, text = '') {
+        let div = document.createElement('div');
+
+        div.classList.add('form__error-element');
+        div.innerText = text;
+
+        event.target.parentElement.append(div);
+      },
+      removeErrorElement(event) {
+        let div = event.target.parentElement.querySelector('.form__error-element')
+
+        if(div) {
+          div.remove();
+        }
+      },
+      checkField(event, match, classError = '', classGood = '', text = '') {
+        let matches = event.target.value.match(match);
+
+        if (!matches) {
+          this.toggleClasses(event, classError, classGood);
+          this.addErrorElement(event, text);
+        } else {
+          this.toggleClasses(event, classGood, classError);
+          this.removeErrorElement(event);
+        }
+      },
+      prepareField(event) {
+        this.labelChange(event);
+
+        let classError = 'error',
+            classGood = 'good';
+
+        if (
+            event.target.value.length > 0
+            || (!event.target.classList.contains(classError) || !event.target.classList.contains(classGood))
+        ) {
+          if (event.target.name === 'form-phone') {
+            this.checkField(event, /^\+7\s\(([0-9]){3}\)-([0-9]){3}(-([0-9]){2}){2}$/, classError, classGood, 'Некорректный номер');
+          }
+
+          if (event.target.name === 'form-name') {
+            this.checkField(event, /^([A-ЯЁ])+([а-яё])+(\s)*([A-ЯЁ][а-яё])*$/, classError, classGood, 'Некорректное имя');
+          }
+        }
+      },
+      sendPost(data, url = '/') {
+        fetch(url).then((response) => {
+          console.log(response);
+        });
+      },
+      prepareForm() {
+        let form = document.querySelector('form'),
+            formData = new FormData(form),
+            countFields = 0;
+
+        for (let pair of formData.entries()) {
+          if (pair[1].length > 0) {
+            countFields += 1;
+          }
+        }
+
+        if (countFields === 3 && document.querySelectorAll('.good').length >= 4) {
+          let params = new URLSearchParams(formData).toString();
+          this.sendPost(formData, form.action + '?' + params);
+        }
       }
-    }
+    },
   }
 </script>
