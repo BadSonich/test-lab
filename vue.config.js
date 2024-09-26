7b3ee7cfe43732acf43d40a77eeae174d151bf95
sonich <s.berdnikova@onpeak.ru>
2024-09-26 18:50:06 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/test-lab/'
+      ? '/badsonich.github.io/test-lab//'
       : '/'
 })
