13fa5b15670d79fb31855b0614df43ec214eb48d
sonich <s.berdnikova@onpeak.ru>
2024-09-26 18:29:28 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:badsonich/test-lab.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
  git push -f git@github.com:BadSonich/test-lab.git main:gh-pages
